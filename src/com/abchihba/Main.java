e854a51304aa0340b3f0c03d85ebf38fb7b60b1f
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-10-23 14:22:11 +0200

    feat: finish task 1

--- src/com/abchihba/Main.java
+++ src/com/abchihba/Main.java
@@ -1,15 +1,19 @@
 package com.abchihba;
 
 import lib.Window;
+import lib.render.Texture;
 
 public class Main extends Window {
+    private final Texture background = Texture.load("background.jpg");
 
     public Main() {
-        super(800, 800, "Test", true, "Cambria Math", 46);
+        super(800, 600, "Абчихба", true, "Arial", 40);
+        setIcon("icon.jpg");
     }
 
     @Override
     protected void onFrame(double elapsed) {
+        canvas.drawTexture(background, 0, 0, width, height);
     }
 
     public static void main(String[] args) {
