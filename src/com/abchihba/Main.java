160d0e1b2d5441a5367d265bf88037957c4bf13e
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-10-23 15:44:15 +0200

    feat: finish task 3

--- src/com/abchihba/Main.java
+++ src/com/abchihba/Main.java
@@ -2,26 +2,58 @@ package com.abchihba;
 
 import lib.Window;
 import lib.render.Texture;
+import org.lwjgl.glfw.GLFW;
 
 import java.awt.*;
 import java.util.ArrayList;
 
 public class Main extends Window {
     private final Texture background = Texture.load("background.jpg");
-    private final int pointsCount;
-    private final ArrayList<Vector> pointsPositions;
+    private int pointsCount;
+    private ArrayList<Vector> pointsPositions;
 
     public Main(int pointsCount) {
         super(800, 600, "Абчихба", true, "Arial", 40);
         setIcon("icon.jpg");
 
         this.pointsCount = pointsCount;
+        createPointsPositions();
+    }
+
+    private void createPointsPositions() {
         this.pointsPositions = new ArrayList<Vector>();
         double angle = 2*Math.PI / this.pointsCount;
         for (int i = 0; i < this.pointsCount; i++) {
             Vector vector = new Vector(200, 0);
             vector.rotate(angle * i);
             this.pointsPositions.add(vector);
+        }
+    }
+
+    @Override
+    protected void onScroll(double dx, double dy) {
+        super.onScroll(dx, dy);
+        this.pointsCount = Math.max(pointsCount + (int) dy, 3);
+        createPointsPositions();
+    }
+
+    @Override
+    protected void onKeyButton(int key, int scancode, int action, int mods) {
+        super.onKeyButton(key, scancode, action, mods);
+        if(action == GLFW.GLFW_RELEASE)
+            return;
+
+        switch (key) {
+            case GLFW.GLFW_KEY_UP: {
+                this.pointsCount = pointsCount + 1;
+                createPointsPositions();
+                break;
+            }
+            case GLFW.GLFW_KEY_DOWN: {
+                this.pointsCount = Math.max(pointsCount - 1, 3);
+                createPointsPositions();
+                break;
+            }
         }
     }
 
@@ -36,8 +68,8 @@ public class Main extends Window {
     }
 
     private void drawPolygon() {
-        double centerX = width / 2;
-        double centerY = height / 2;
+        double centerX = width / 2.;
+        double centerY = height / 2.;
 
         for(int side = 0; side < pointsPositions.size(); side++) {
             Vector start = getPoint(side);;
